f5f4c0d4674590ec63f8d8e195dd8c6a2cff718d
alxstudent1 <122865688+alxstudent1@users.noreply.github.com>
2024-08-02 14:13:03 +0000

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
-result = add(a, b)
 if __name__ == "__main__":
-    print("{} + {} = {}".format(1, 2, result))
+    from add_0 import add
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{:d} + {:d} = {:d}".format(1, 2, result))
