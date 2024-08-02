4e2db6b0aeedc11ac8cd3f395b0dd7ba556f2d39
alxstudent1 <122865688+alxstudent1@users.noreply.github.com>
2024-08-02 18:40:26 +0000

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,12 @@
 if __name__ == "__main__":
     from sys import argv
     if len(argv) == 2:
-        print("{:d} argument:".format(len(argv) - 1))
+        print("{} argument:".format(len(argv) - 1))
         for index, value in enumerate(argv[1 : ], start=1):
-            print("{:d}: {:s}".format(index, value))
+            print("{}: {}".format(index, value))
     elif len(argv) > 2:
-        print("{:d} arguments:".format(len(argv)))
+        print("{} arguments:".format(len(argv)))
         for index, value in enumerate(argv[1 : ], start=1):
-            print("{:d}: {:s}".format(index, value))            
+            print("{}: {}".format(index, value))            
     else:
         print("0 arguments.")
