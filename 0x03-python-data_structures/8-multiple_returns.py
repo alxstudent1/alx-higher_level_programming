020c840e62ae0e64781bab2c10f63b1f3371416f
alxstudent1 <122865688+alxstudent1@users.noreply.github.com>
2024-08-09 12:13:04 +0000

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     taille = len(sentence)
-    firs_char = sentence[0]
     if taille == 0:
         firs_char = None
         couple = (taille, first_char)
         return couple
     else:
+        first_char = sentence[0]
+        couple = (taille, first_char)
         return couple
